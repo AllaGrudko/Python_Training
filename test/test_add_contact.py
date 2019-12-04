581a2cd8bc1f004d7ce877d03abaec6509121715
Alla <liliya86@inbox.ru>
2019-12-04 13:15:26 +0300

    2. Задание №5: Реализовать двухуровневую архитектуру тестов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,15 +12,15 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="sdfsf", lastname="werwre", address="fdgdg", home_phone="343",
-                            mobile_phone="453", work_phone="345", fax="345", email="xcxcx", email2="xcvv",
-                            email3="xcvv", homepage="dsfdsf"))
+    app.contact.create(Contact(firstname="sdfsf", lastname="werwre", address="fdgdg", home_phone="343",
+                               mobile_phone="453", work_phone="345", fax="345", email="xcxcx", email2="xcvv",
+                               email3="xcvv", homepage="dsfdsf"))
     app.session.logout()
 
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="", lastname="", address="", home_phone="", mobile_phone="",
-                            work_phone="", fax="", email="", email2="", email3="", homepage=""))
+    app.contact.create(Contact(firstname="", lastname="", address="", home_phone="", mobile_phone="",
+                               work_phone="", fax="", email="", email2="", email3="", homepage=""))
     app.session.logout()
 
