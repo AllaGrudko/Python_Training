d7738040bf0b8a7c3e73c226aec65111c6554840
Alla <liliya86@inbox.ru>
2019-12-06 14:59:40 +0300

    2. Задание №7: Сделаны тесты создания, удаления, модификации для групп и контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -45,6 +45,46 @@ class ContactHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_home_page()
 
+    def delete_first_contact(self):
+        wd = self.app.wd
+        # select first contact
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to_alert().accept()
+       # wd.find_element_by_name("selected[]")
+       # self.return_to_home_page()
+
+    def edit_first_contact(self):
+        wd = self.app.wd
+        # click edit to change contact
+        wd.find_element_by_css_selector("img[alt=\"Edit\"]").click()
+        # change contact
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys("new firstname")
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys("new lastname")
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys("new address")
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys("new home")
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys("new mobile")
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys("new email")
+        # submit change
+        wd.find_element_by_name("update").click()
+        self.return_to_home_page()
+
     def return_to_home_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
+
