fc6d192e5d72daaa66f8c004e78df83c9b27124c
Alla <liliya86@inbox.ru>
2019-12-11 17:55:11 +0300

    Валидация фикстуры перед каждым тестом

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,16 +3,12 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="sdfsf", lastname="werwre", address="fdgdg", home_phone="343",
                                mobile_phone="453", work_phone="345", fax="345", email="xcxcx", email2="xcvv",
                                email3="xcvv", homepage="dsfdsf"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", lastname="", address="", home_phone="", mobile_phone="",
                                work_phone="", fax="", email="", email2="", email3="", homepage=""))
-    app.session.logout()
 
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,16 +2,10 @@
 from model.contact import Contact
 
 def test_edit_first_contact_firstname(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(Contact(firstname="kkkk"))
-    app.session.logout()
 
 def test_edit_first_contact_lastname(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(Contact(lastname="aaaaaa"))
-    app.session.logout()
 
 def test_edit_first_contact_address(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(Contact(address="ccccc"))
-    app.session.logout()
