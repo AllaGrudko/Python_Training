37752eac0581b9b3a5a3e7aefc5b612c0ce79f40
Alla <liliya86@inbox.ru>
2020-01-23 11:47:55 +0300

    динамическая генерация тестов

---
+++ data/contacts.py
@@ -0,0 +1,10 @@
+from model.contact import Contact
+
+testdata = [
+    Contact (firstname="firstname1", lastname="lastname1", address="address1", home_phone="home_phone1",
+             mobile_phone="mobile_phone1", work_phone="work_phone1", fax="fax1", email="email1", email2="email21",
+             email3="email31", homepage="homepage1"),
+    Contact (firstname="firstname2", lastname="lastname2", address="address2", home_phone="home_phone2",
+             mobile_phone="mobile_phone2", work_phone="work_phone2", fax="fax2", email="email2", email2="email22",
+             email3="email32", homepage="homepage2")
+]
