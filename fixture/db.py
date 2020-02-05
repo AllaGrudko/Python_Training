bbba56dac6dafa9e1dab286bad736d846e1c57fb
Alla <liliya86@inbox.ru>
2020-02-05 13:12:15 +0300

    7. Задание №21: Переделать тесты для проверки информации о контактах на главной странице

--- fixture/db.py
+++ fixture/db.py
@@ -27,10 +27,11 @@ class DbFixture:
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname, address, home, homepage  from addressbook where deprecated='0000-00-00 00:00:00'")
+            cursor.execute("select id, firstname, lastname, address, home, homepage, mobile, work, phone2, email, email2, email3  from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
-                (id, firstname, lastname, address, home, homepage) = row
-                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, address=address, home_phone=home, homepage=homepage))
+                (id, firstname, lastname, address, home, homepage, mobile, work, phone2, email, email2, email3) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, address=address, home_phone=home, homepage=homepage,
+                                    mobile_phone=mobile, work_phone=work, secondary_phone=phone2, email=email, email2=email2, email3=email3))
 
         finally:
             cursor.close()
